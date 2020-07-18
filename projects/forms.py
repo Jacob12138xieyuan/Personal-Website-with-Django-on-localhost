20735a9e52fc818ad8c4ddc333308bfcc7b47ff1
Jacob12138xieyuan <xiey0017@ntu.edu.sg>
2020-07-18 22:08:03 +0800

    uniform UI

--- projects/forms.py
+++ projects/forms.py
@@ -5,8 +5,5 @@ from .models import Project
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['name', 'image', 'summary', 'detail', 'submission_date']
-        field_classes = {
-            'image': forms.ImageField,
-            'submission_date': forms.DateTimeField
-        }
+        fields = ['name', 'image', 'summary',
+                  'detail', 'github_link', 'submission_date']
--- projects/models.py
+++ projects/models.py
@@ -8,4 +8,7 @@ class Project(models.Model):
     image = models.ImageField(upload_to='image/')
     summary = models.TextField()
     detail = models.TextField()
-    submission_date = models.DateTimeField()
+    github_link = models.CharField(
+        blank=True, max_length=100)
+    submission_date = models.DateTimeField(
+        help_text='e.g. 2020-07-16 06:35:08')
